ee2a0df8dbc9fddd3bebfd52b67910de06b6ddd6
Unai Fuentes <hate463@gmail.com>
2025-03-04 18:25:55 +0100

    Change route name

--- src/components/products/products-list.tsx
+++ src/components/products/products-list.tsx
@@ -9,9 +9,9 @@ type ProductsProperties = {
 };
 
 export async function ProductList({ page, search }: ProductsProperties) {
-  const phones = await getAllProducts({ page, search });
+  const products = await getAllProducts({ page, search });
 
-  if (phones && phones.length <= 0) {
+  if (products && products.length <= 0) {
     return (
       <p className="text-center text-base my-12 uppercase">No results found</p>
     );
@@ -19,10 +19,10 @@ export async function ProductList({ page, search }: ProductsProperties) {
 
   return (
     <>
-      <p className="uppercase text-xs mt-3">{phones.length} RESULTS</p>
+      <p className="uppercase text-xs mt-3">{products.length} RESULTS</p>
       <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-5 my-12">
-        {phones.map(({ basePrice, brand, imageUrl, id, name }, index) => (
-          <Link href={`/phone/${id}`} key={index}>
+        {products.map(({ basePrice, brand, imageUrl, id, name }, index) => (
+          <Link href={`/product/${id}`} key={index}>
             <ProductCard
               key={index}
               basePrice={basePrice}
