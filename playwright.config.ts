3c856f0b3903d3c60b665bfca6b1bb5c187d6ac7
Unai Fuentes <hate463@gmail.com>
2025-03-06 10:57:39 +0100

    Remove webkit temporally https://github.com/microsoft/playwright/issues/33057

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,10 +46,12 @@ export default defineConfig({
       use: { ...devices["Desktop Firefox"] },
     },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    /* Test against Apple Silicon. */
+
+    // {
+    //  name: "webkit",
+    //  use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
